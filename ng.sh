0925711594658b39e558cfdca0888710da3b3944
oussamabriedj <133027454+oussamabriedj@users.noreply.github.com>
2023-05-27 00:03:51 +0200

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -5,4 +5,4 @@ echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
  
-./ngrok authtoken 1ySza0hlF4uTzAQe6XLhFgFvopB_5wUZpCV8VWGQXGAnxj7yS
+./ngrok authtoken 
