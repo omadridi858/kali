3c9ec2553779db8ba34672ee2b51fa8031fe4f6b
oussamabriedj <133027454+oussamabriedj@users.noreply.github.com>
2023-05-27 00:18:30 +0200

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -4,7 +4,7 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-export NGROK_AUTH_TOKEN="<your-ngrok-auth-token>"
+
 
 # Execute ngrok with the authtoken
-./ngrok authtoken "$NGROK_AUTH_TOKEN"
+./ngrok authtoken $1
