03ec0752e60b6f868d498e4147da71f15137d8df
oussamabriedj <133027454+oussamabriedj@users.noreply.github.com>
2023-05-26 23:52:20 +0200

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,6 +1,6 @@
 echo -e "$123\n$123\n" | sudo passwd
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/3sPrc9S > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/oussamabriedj/macOS/main/ng.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
